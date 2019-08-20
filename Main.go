999b4ed92d7b52ee4439d7388fc57e76caed9820
Atharva <avu.ceo@gmail.com>
2019-08-20 21:45:30 +0530

    Crypto BLockchain Added

--- Main.go
+++ Main.go
@@ -20,7 +20,7 @@ type Block struct {
 	Nonce     uint64
 	Timestamp int64
 	Index     uint64
-	Validate bool
+	Validate  bool
 }
 type Valid struct {
 	Status bool
@@ -30,11 +30,10 @@ type dem struct {
 	Str string
 }
 type TransInfo struct {
-	Sender string
+	Sender  string
 	Reciver string
-	Amount uint64
-	Miner string
-
+	Amount  uint64
+	Miner   string
 }
 type CryptoBlock struct {
 	PrevHash  string
@@ -42,15 +41,22 @@ type CryptoBlock struct {
 	Nonce     uint64
 	Timestamp int64
 	Index     uint64
-	Validate bool
-	Data TransInfo
-
+	Validate  bool
+	Data      TransInfo
 }
 
-var bchain [] Block
-var cryptochain [] CryptoBlock
+type Wallet struct {
+	Name    string
+	Amount  uint64
+	Address string
+}
 
-var diff uint64=3;
+var bchain []Block
+var cryptochain []CryptoBlock
+var wallets []Wallet
+
+var diff uint64 = 3
+var cryptodiff uint64 = 3
 
 func main() {
 	port := os.Getenv("PORT")
@@ -66,6 +72,22 @@ func main() {
 	router.Use(gin.Logger())
 	router.LoadHTMLGlob("templates/*")
 
+	info := TransInfo{"0x000000", "0x111111", 50, "0x222222"}
+	cryptochain = CryptoAddBlock(info, "0", cryptochain)
+
+	info = TransInfo{"0x000000", "0x222222", 50, "0x111111"}
+	cryptochain = CryptoAddBlock(info, cryptochain[0].CurrHash, cryptochain)
+
+	info = TransInfo{"0x000000", "0x333333", 50, "0x222222"}
+	cryptochain = CryptoAddBlock(info, cryptochain[1].CurrHash, cryptochain)
+
+	steve := Wallet{"Steve", 0, "0x111111"}
+	mark := Wallet{"Mark", 0, "0x222222"}
+	bill := Wallet{"Bill", 0, "0x333333"}
+
+	wallets = append(wallets, steve)
+	wallets = append(wallets, mark)
+	wallets = append(wallets, bill)
 
 	router.GET("/", func(c *gin.Context) {
 
@@ -75,24 +97,19 @@ func main() {
 		c.JSON(200, gin.H{"blockchain": bchain})
 	})
 	router.GET("/reset", func(c *gin.Context) {
-		bchain = [] Block{};
-
-
+		bchain = []Block{}
 		c.JSON(200, gin.H{"blockchain": bchain})
 	})
 	router.GET("/addblock", func(c *gin.Context) {
 
-
-		if(len(bchain)==0){
+		if len(bchain) == 0 {
 			bchain = AddBlock("0", "0", bchain)
 
-		}else{
+		} else {
 			fmt.Println(c.Query("ids"))
 			bchain = AddBlock(bchain[len(bchain)-1].CurrHash, c.Query("data"), bchain)
 
 		}
-
-
 
 		c.JSON(200, gin.H{"blockchain": bchain})
 	})
@@ -108,7 +125,7 @@ func main() {
 
 	router.GET("/validate", func(c *gin.Context) {
 		ReMine(bchain)
-		bchain= ValidateChain(bchain)
+		bchain = ValidateChain(bchain)
 		c.JSON(200, gin.H{"blockchain": bchain})
 	})
 
@@ -117,27 +134,42 @@ func main() {
 			"title": "Main website",
 		})
 	})
+	router.GET("/cryptodemo", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "crypto.html", gin.H{
+			"title": "Main website",
+		})
+	})
+
 	router.GET("/changediff", func(c *gin.Context) {
 
 		i, err := strconv.Atoi(c.Query("diff"))
-		diff= uint64(i);
-		fmt.Println(err);
-
-	c.JSON(200, gin.H{"blockchain": bchain})
+		diff = uint64(i)
+		fmt.Println(err)
+		c.JSON(200, gin.H{"blockchain": bchain})
 
 	})
+	router.GET("/cryptoreset", func(c *gin.Context) {
+		cryptochain = []CryptoBlock{}
+		info := TransInfo{"0x000000", "0x111111", 50, "0x222222"}
+		cryptochain = CryptoAddBlock(info, "0", cryptochain)
 
+		info = TransInfo{"0x000000", "0x222222", 50, "0x111111"}
+		cryptochain = CryptoAddBlock(info, cryptochain[0].CurrHash, cryptochain)
 
+		info = TransInfo{"0x000000", "0x333333", 50, "0x222222"}
+		cryptochain = CryptoAddBlock(info, cryptochain[1].CurrHash, cryptochain)
 
+		c.JSON(200, gin.H{"blockchain": cryptochain})
+	})
 
 	router.GET("/setdata", func(c *gin.Context) {
 
 		i, err := strconv.Atoi(c.Query("index"))
 
-		j:=c.Query("data")
+		j := c.Query("data")
 
-		fmt.Println(err);
-		SetBlock(bchain, uint64(i),j)
+		fmt.Println(err)
+		SetBlock(bchain, uint64(i), j)
 
 		c.JSON(200, gin.H{"blockchain": bchain})
 
@@ -146,16 +178,52 @@ func main() {
 	router.GET("/crypto", func(c *gin.Context) {
 
 		c.JSON(200, gin.H{"blockchain": cryptochain})
+
 	})
 
 	router.GET("/cryptosetdata", func(c *gin.Context) {
 
-		cryptochain=CryptoAddBlock(TransInfo{"A","B",111,"C"},"@",cryptochain);
+		i, err := strconv.Atoi(c.Query("index"))
+		amnt, erri := strconv.Atoi(c.Query("amount"))
+		fmt.Println(i, err, erri)
+		info := TransInfo{c.Query("sender"), c.Query("reciver"), uint64(amnt), c.Query("miner")}
+		cryptochain = CryptoSetBlock(cryptochain, uint64(i), info)
+
+		c.JSON(200, gin.H{"blockchain": cryptochain})
+
+	})
+
+	router.GET("/cryptoadd", func(c *gin.Context) {
+
+		amnt, erri := strconv.Atoi(c.Query("amount"))
+		fmt.Println(erri)
+		info := TransInfo{c.Query("sender"), c.Query("reciver"), uint64(amnt), c.Query("miner")}
+		cryptochain = CryptoAddBlock(info, c.Query("prevhash"), cryptochain)
+		c.JSON(200, gin.H{"blockchain": cryptochain})
+
+	})
+
+	router.GET("/cryptovalidate", func(c *gin.Context) {
+		CryptoReMine(cryptochain)
+		cryptochain = CryptoValidateChain(cryptochain)
 		c.JSON(200, gin.H{"blockchain": cryptochain})
 	})
 
+	router.GET("/cryptochangediff", func(c *gin.Context) {
 
+		i, err := strconv.Atoi(c.Query("diff"))
+		cryptodiff = uint64(i)
+		fmt.Println(err)
+		c.JSON(200, gin.H{"blockchain": cryptochain})
 
+	})
+
+	router.GET("/wallets", func(c *gin.Context) {
+
+		wallets = CryptoCalculate(cryptochain)
+		c.JSON(200, gin.H{"wallet": wallets})
+
+	})
 
 	router.Run(":" + port)
 }
@@ -211,7 +279,7 @@ func AddBlock(PrevHash string, Data string, blockchain []Block) []Block {
 	b.Data = Data
 	msec := time.Now().UnixNano() / 1000000
 	b.Timestamp = msec
-	b.Validate=true;
+	b.Validate = true
 	blockchain = append(blockchain, b)
 
 	return blockchain
@@ -235,7 +303,6 @@ func ReMine(blockchain []Block) {
 		blockchain[i] = b
 	}
 
-
 }
 
 func ValidateChain(blockchain []Block) []Block {
@@ -244,13 +311,12 @@ func ValidateChain(blockchain []Block) []Block {
 	for i := 0; i < len(blockchain)-1; i++ {
 
 		if blockchain[i].CurrHash == blockchain[i+1].PrevHash {
-			blockchain[i].Validate=true;
-
+			blockchain[i].Validate = true
 		} else {
 
-			for j:=i;j<len(blockchain);j++{
+			for j := i; j < len(blockchain); j++ {
 
-				blockchain[j].Validate=false;
+				blockchain[j].Validate = false
 			}
 			Vali.Index = uint64(i)
 			Vali.Status = false
@@ -258,13 +324,12 @@ func ValidateChain(blockchain []Block) []Block {
 		}
 	}
 
-	return blockchain;
+	return blockchain
 }
-
 
 ///ADD BLOCK TO CRYPTOCHAIN
 
-func CryptoAddBlock(Data TransInfo,Prevhash string, blockchain []CryptoBlock) []CryptoBlock {
+func CryptoAddBlock(Data TransInfo, Prevhash string, blockchain []CryptoBlock) []CryptoBlock {
 
 	var buffer bytes.Buffer
 
@@ -277,20 +342,17 @@ func CryptoAddBlock(Data TransInfo,Prevhash string, blockchain []CryptoBlock) []
 	b.PrevHash = Prevhash
 	b.Data = Data
 	b.Timestamp = msec
-	b.Validate=true;
+	b.Validate = true
 	blockchain = append(blockchain, b)
 
 	return blockchain
 }
 
-
-
-
 func ProofOfWork2(str string) CryptoBlock {
 
 	b := CryptoBlock{}
 	var non bytes.Buffer
-	for i := 0; i < int(diff); i++ {
+	for i := 0; i < int(cryptodiff); i++ {
 		non.WriteString("0")
 	}
 	i := 0
@@ -314,17 +376,8 @@ func ProofOfWork2(str string) CryptoBlock {
 		i++
 	}
 
-	return b;
+	return b
 }
-
-///CHANGE CRYPTOCHAIN
-
-func CryptoSetBlock(blockchain []CryptoBlock, pos uint64, Data TransInfo) []CryptoBlock {
-
-	blockchain[pos].Data = Data
-	return blockchain
-}
-
 
 func CryptoValidateChain(blockchain []CryptoBlock) []CryptoBlock {
 	Vali := Valid{}
@@ -332,13 +385,12 @@ func CryptoValidateChain(blockchain []CryptoBlock) []CryptoBlock {
 	for i := 0; i < len(blockchain)-1; i++ {
 
 		if blockchain[i].CurrHash == blockchain[i+1].PrevHash {
-			blockchain[i].Validate=true;
-
+			blockchain[i].Validate = true
 		} else {
 
-			for j:=i;j<len(blockchain);j++{
+			for j := i; j < len(blockchain); j++ {
 
-				blockchain[j].Validate=false;
+				blockchain[j].Validate = false
 			}
 			Vali.Index = uint64(i)
 			Vali.Status = false
@@ -346,8 +398,9 @@ func CryptoValidateChain(blockchain []CryptoBlock) []CryptoBlock {
 		}
 	}
 
-	return blockchain;
+	return blockchain
 }
+
 /////REMINE
 
 func CryptoReMine(blockchain []CryptoBlock) {
@@ -365,7 +418,79 @@ func CryptoReMine(blockchain []CryptoBlock) {
 		blockchain[i] = b
 	}
 
-
 }
 
+func CryptoSetBlock(blockchain []CryptoBlock, pos uint64, Data TransInfo) []CryptoBlock {
 
+	blockchain[pos].Data = Data
+	return blockchain
+}
+
+func CryptoCalculate(blockchain []CryptoBlock) []Wallet {
+
+	var s uint64 = 0
+	var b uint64 = 0
+	var m uint64 = 0
+
+	for i := 0; i < len(blockchain)-1; i++ {
+
+		if blockchain[i].Data.Reciver == "0x111111" {
+			s = s + blockchain[i].Data.Amount
+			wallets[0].Amount = s
+
+		}
+
+		if blockchain[i].Data.Reciver == "0x222222" {
+			m = m + blockchain[i].Data.Amount
+			wallets[1].Amount = m
+
+		}
+		if blockchain[i].Data.Reciver == "0x333333" {
+			b = b + blockchain[i].Data.Amount
+			wallets[2].Amount = b
+
+		}
+
+		////////////////////////////////////
+
+		if blockchain[i].Data.Sender == "0x111111" {
+			s = s - blockchain[i].Data.Amount
+			wallets[0].Amount = s
+
+		}
+
+		if blockchain[i].Data.Sender == "0x222222" {
+
+			m = m - blockchain[i].Data.Amount
+			wallets[1].Amount = m
+
+		}
+		if blockchain[i].Data.Sender == "0x333333" {
+			b = b - blockchain[i].Data.Amount
+			wallets[2].Amount = b
+
+		}
+
+		//////////////////////////////////////
+
+		if blockchain[i].Data.Miner == "0x111111" {
+			s++
+			wallets[0].Amount = s
+
+		}
+
+		if blockchain[i].Data.Miner == "0x222222" {
+			m++
+			wallets[1].Amount = m
+
+		}
+		if blockchain[i].Data.Miner == "0x333333" {
+			b++
+			wallets[2].Amount = b
+
+		}
+
+	}
+	return wallets
+
+}
